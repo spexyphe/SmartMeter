0e4d730244b3b57b7752d9a97fcfd8f8e63c32bc
XyPHe <Marcel.j.m.Koolen@gmail.com>
2022-03-12 22:14:13 +0100

    import outside of test

--- Project/test/unit/test_main_smart_meter.py
+++ Project/test/unit/test_main_smart_meter.py
@@ -7,6 +7,22 @@ from pathlib import Path
 
 from pip import main
 
+file = Path(__file__).resolve()
+parent, top = file.parent, file.parents[3]
+
+sys.path.append(str(top))
+
+try:
+    sys.path.remove(str(parent))
+except ValueError: # Already removed
+    pass
+
+import Project.test.unit
+__package__ = 'Project.test.unit'
+
+from ... import main_smart_meter as meter
+
+
 class TestStringMethods(unittest.TestCase):
 
                                                                                                                         
@@ -93,21 +109,6 @@ class TestStringMethods(unittest.TestCase):
 
 
 if __name__ == '__main__':
-
-    file = Path(__file__).resolve()
-    parent, top = file.parent, file.parents[3]
-
-    sys.path.append(str(top))
-
-    try:
-        sys.path.remove(str(parent))
-    except ValueError: # Already removed
-        pass
-
-    import Project.test.unit
-    __package__ = 'Project.test.unit'
-
-    from ... import main_smart_meter as meter
 
     unittest.main()
 
